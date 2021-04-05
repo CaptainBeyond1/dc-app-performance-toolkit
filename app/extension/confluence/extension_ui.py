f6e52d6062d7a4a99e7ea5aa7f3e782c986b47b9
Alexandr Tkachenko <otkachenko@ukrinsoft.net>
2021-04-05 15:56:29 +0300

    custom action

--- app/extension/confluence/extension_ui.py
+++ app/extension/confluence/extension_ui.py
@@ -1,44 +1,50 @@
 import random
 
 from selenium.webdriver.common.by import By
-
+from selenium.webdriver import ActionChains
 from selenium_ui.base_page import BasePage
 from selenium_ui.conftest import print_timing
-from selenium_ui.confluence.pages.pages import Login, AllUpdates
+from selenium.webdriver.common.keys import Keys
+from selenium_ui.confluence.pages.pages import Login, AllUpdates, Editor
+from selenium_ui.confluence.pages.selectors import UrlManager, LoginPageLocators, AllUpdatesLocators, PopupLocators,\
+    PageLocators, DashboardLocators, TopPanelLocators, EditorLocators, LogoutLocators
 from util.conf import CONFLUENCE_SETTINGS
 
 
 def app_specific_action(webdriver, datasets):
-    page = BasePage(webdriver)
+    page = BasePage(webdriver);
     if datasets['custom_pages']:
         app_specific_page_id = datasets['custom_page_id']
 
-    # To run action as specific user uncomment code bellow.
-    # NOTE: If app_specific_action is running as specific user, make sure that app_specific_action is running
-    # just before test_2_selenium_z_log_out
-    # @print_timing("selenium_app_specific_user_login")
-    # def measure():
-    #     def app_specific_user_login(username='admin', password='admin'):
-    #         login_page = Login(webdriver)
-    #         login_page.delete_all_cookies()
-    #         login_page.go_to()
-    #         login_page.wait_for_page_loaded()
-    #         login_page.set_credentials(username=username, password=password)
-    #         login_page.click_login_button()
-    #         if login_page.is_first_login():
-    #             login_page.first_user_setup()
-    #         all_updates_page = AllUpdates(webdriver)
-    #         all_updates_page.wait_for_page_loaded()
-    #     app_specific_user_login(username='admin', password='admin')
-    # measure()
-
     @print_timing("selenium_app_custom_action")
     def measure():
-
-        @print_timing("selenium_app_custom_action:view_page")
+        specific_edit_page = Editor(webdriver, page_id=datasets['custom_page_id'])
+        specific_edit_page.go_to()
+        @print_timing("selenium_app_custom_action:copy and paste styles")
         def sub_measure():
-            page.go_to_url(f"{CONFLUENCE_SETTINGS.server_url}/pages/viewpage.action?pageId={app_specific_page_id}")
-            page.wait_until_visible((By.ID, "title-text"))  # Wait for title field visible
-            page.wait_until_visible((By.ID, "ID_OF_YOUR_APP_SPECIFIC_UI_ELEMENT"))  # Wait for you app-specific UI element by ID selector
+            actions = ActionChains(webdriver)
+            specific_edit_page = Editor(webdriver, page_id=datasets['custom_page_id'])
+            specific_edit_page.wait_until_visible((By.ID, "format-painter-button"))
+            specific_edit_page.wait_until_available_to_switch(EditorLocators.page_content_field)
+            editor_field = specific_edit_page.get_element((By.ID, "tinymce"))
+            editor_field.click()
+            actions.send_keys(Keys.HOME).perform();
+            actions.key_down(Keys.LEFT_SHIFT).send_keys(Keys.ARROW_RIGHT).key_up(Keys.LEFT_SHIFT).perform();
+            specific_edit_page.return_to_parent_frame()
+            specific_edit_page.get_element((By.ID, "format-painter-button")).click()
+            specific_edit_page.wait_until_available_to_switch(EditorLocators.page_content_field)
+            editor_field.click();
+            actions.send_keys(Keys.HOME).perform();
+            actions.send_keys(Keys.ARROW_RIGHT).perform()
+
+            actions.key_down(Keys.LEFT_SHIFT).perform();
+            for x in range(5):
+                actions.send_keys(Keys.ARROW_RIGHT).perform()
+            actions.key_up(Keys.LEFT_SHIFT).perform();
+            actions.key_down(Keys.CONTROL).key_down(Keys.ALT).send_keys("p").key_up(Keys.CONTROL).key_up(Keys.ALT).perform();
         sub_measure()
+        specific_edit_page.return_to_parent_frame();
+        specific_edit_page.save_edited_page();
     measure()
+
+
