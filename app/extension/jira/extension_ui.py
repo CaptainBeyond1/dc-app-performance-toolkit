df49371d6db7a9dfd4d37bca62e2f75ab62ea442
Vitaly Nezdvetsky <vnezdd@gmail.com>
2021-06-05 16:11:16 +0300

    fixed custom action

--- app/extension/jira/extension_ui.py
+++ app/extension/jira/extension_ui.py
@@ -41,8 +41,9 @@ def app_specific_action(webdriver, datasets):
             page.wait_until_visible((By.ID, "summary"))
             page.get_element((By.ID, "summary")).click()
             actions.send_keys(''.join(random.choice(letters) for i in range(10))).perform();
+            page.get_element((By.ID, "reporter-field")).click()
             page.get_element((By.ID, "create-issue-submit")).click()
+            page.wait_until_invisible((By.ID, "create-issue-submit"))
         sub_measure()
     measure()
-
 
