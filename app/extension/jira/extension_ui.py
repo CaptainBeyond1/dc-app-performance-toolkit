fbd892688c0b9775c1ba9d87d57704d50633594f
Vitaly Nezdvetsky <vnezdd@gmail.com>
2021-06-05 13:09:28 +0300

    fixed custom action

--- app/extension/jira/extension_ui.py
+++ app/extension/jira/extension_ui.py
@@ -15,18 +15,35 @@ def app_specific_action(webdriver, datasets):
     page = BasePage(webdriver)
     letters = string.ascii_lowercase
 
+    @print_timing("selenium_app_specific_user_login")
+    def measure():
+        def app_specific_user_login(username='admin', password='admin'):
+            login_page = Login(webdriver)
+            login_page.delete_all_cookies()
+            login_page.go_to()
+            login_page.set_credentials(username=username, password=password)
+            if login_page.is_first_login():
+                login_page.first_login_setup()
+            if login_page.is_first_login_second_page():
+                login_page.first_login_second_page_setup()
+            login_page.wait_for_page_loaded()
+        app_specific_user_login(username='admin', password='admin')
+    measure()
+
     @print_timing("selenium_app_custom_action")
     def measure():
-        @print_timing("selenium_app_custom_action:create_issue_from_link")
+        @print_timing("selenium_app_custom_action:view_issue")
         def sub_measure():
             actions = ActionChains(webdriver)
-            page.go_to_url(f"{JIRA_SETTINGS.server_url}/secure/CallViewAction.jspa?reporter={datasets['username']}")
+            page.go_to_url(f"{JIRA_SETTINGS.server_url}/secure/CallViewAction.jspa?reporter=admin")
             page.wait_until_visible((By.ID, "create_link"))
             page.get_element((By.ID, "create_link")).click()
             page.wait_until_visible((By.ID, "summary"))
             page.get_element((By.ID, "summary")).click()
             actions.send_keys(''.join(random.choice(letters) for i in range(10))).perform();
             page.get_element((By.ID, "create-issue-submit")).click()
+            page.wait_until_invisible((By.ID, "create-issue-submit"))
         sub_measure()
     measure()
+
 
