b2c24069e68d27ed1db38f4ffb558f99f46117a0
Vitaly Nezdvetsky <vnezdd@gmail.com>
2021-08-17 20:35:50 +0300

    navi test

--- app/extension/confluence/extension_ui.py
+++ app/extension/confluence/extension_ui.py
@@ -1,44 +1,34 @@
 import random
 
 from selenium.webdriver.common.by import By
-
+from selenium.webdriver import ActionChains
 from selenium_ui.base_page import BasePage
 from selenium_ui.conftest import print_timing
-from selenium_ui.confluence.pages.pages import Login, AllUpdates
+from selenium.webdriver.common.keys import Keys
+from selenium_ui.confluence.pages.pages import Login, AllUpdates, Page
+from selenium_ui.confluence.pages.selectors import UrlManager, LoginPageLocators, AllUpdatesLocators, PopupLocators, \
+    PageLocators, DashboardLocators, TopPanelLocators, EditorLocators, LogoutLocators
 from util.conf import CONFLUENCE_SETTINGS
 
 
 def app_specific_action(webdriver, datasets):
-    page = BasePage(webdriver)
-    if datasets['custom_pages']:
-        app_specific_page_id = datasets['custom_page_id']
-
-    # To run action as specific user uncomment code bellow.
-    # NOTE: If app_specific_action is running as specific user, make sure that app_specific_action is running
-    # just before test_2_selenium_z_log_out
-    # @print_timing("selenium_app_specific_user_login")
-    # def measure():
-    #     def app_specific_user_login(username='admin', password='admin'):
-    #         login_page = Login(webdriver)
-    #         login_page.delete_all_cookies()
-    #         login_page.go_to()
-    #         login_page.wait_for_page_loaded()
-    #         login_page.set_credentials(username=username, password=password)
-    #         login_page.click_login_button()
-    #         if login_page.is_first_login():
-    #             login_page.first_user_setup()
-    #         all_updates_page = AllUpdates(webdriver)
-    #         all_updates_page.wait_for_page_loaded()
-    #     app_specific_user_login(username='admin', password='admin')
-    # measure()
+    page = BasePage(webdriver);
 
     @print_timing("selenium_app_custom_action")
     def measure():
-
-        @print_timing("selenium_app_custom_action:view_page")
+        specific_edit_page = Page(webdriver, page_id=datasets['custom_page_id'])
+        specific_edit_page.go_to()
+        #Going to editor iframe
+        @print_timing("selenium_app_custom_action:copy and paste styles")
         def sub_measure():
-            page.go_to_url(f"{CONFLUENCE_SETTINGS.server_url}/pages/viewpage.action?pageId={app_specific_page_id}")
-            page.wait_until_visible((By.ID, "title-text"))  # Wait for title field visible
-            page.wait_until_visible((By.ID, "ID_OF_YOUR_APP_SPECIFIC_UI_ELEMENT"))  # Wait for you app-specific UI element by ID selector
+            specific_edit_page = Page(webdriver, page_id=datasets['custom_page_id'])
+            specific_edit_page.wait_until_visible((By.ID, "navi-next-step"))
+            specific_edit_page.wait_until_clickable((By.ID, "navi-next-step"))
+            next_step_button = specific_edit_page.get_element((By.ID, "navi-next-step"))
+            next_step_button.click()
+            specific_edit_page.wait_until_visible((By.ID, "navi-next-page"))
+            next_page_button = specific_edit_page.get_element((By.ID, "navi-next-page"))
+            specific_edit_page.wait_until_clickable((By.ID, "navi-next-page"))
+            next_page_button.click()
         sub_measure()
     measure()
