df4cbced529cd468120a0f845f0aafe169ed9c64
Vitaly Nezdvetsky <vnezdd@gmail.com>
2021-10-03 15:42:20 +0300

    create another custom action

--- app/extension/jsm/extension_ui_customers.py
+++ app/extension/jsm/extension_ui_customers.py
@@ -18,23 +18,30 @@ def app_specific_action(webdriver, datasets):
     @print_timing("selenium_customer_app_custom_action")
     def measure():
 
-        @print_timing("selenium_customer_app_custom_action:create_another_request")
+        @print_timing("selenium_customer_app_custom_action:view_request")
         def sub_measure():
             action = ActionChains(webdriver)
             page.go_to_url(f"{JSM_SETTINGS.server_url}/servicedesk/customer/portal/86/create/882")
             page.wait_until_visible((By.ID, "summary"))
             name = page.get_element((By.ID, "summary"))
             name.send_keys(''.join(random.choice(letters) for i in range(10)));
+            webdriver.save_screenshot('1.png')
             page.wait_until_visible((By.ID, "customfield_10600"))
             testfield = page.get_element((By.ID, "customfield_10600"))
             testfield.click()
+            webdriver.save_screenshot('2.png')
             testfield.send_keys(''.join(random.choice(letters) for i in range(10)))
+            webdriver.save_screenshot('3.png')
             page.wait_until_visible((By.ID, "create-another-button"))
             createanother = page.get_element((By.ID, "create-another-button"))
             createanother.click()
             page.wait_until_visible((By.ID, 'create-another-button'))
+            webdriver.save_screenshot('final.png')
             action.key_down(Keys.CONTROL).key_down(Keys.TAB).key_up(Keys.CONTROL).key_up(Keys.TAB).perform();
+            webdriver.save_screenshot('final1.png')
             action.key_down(Keys.CONTROL).key_down(Keys.TAB).key_up(Keys.CONTROL).key_up(Keys.TAB).perform();
+            webdriver.save_screenshot('final2.png')
             action.key_down(Keys.CONTROL).key_down(Keys.TAB).key_up(Keys.CONTROL).key_up(Keys.TAB).perform();
+            webdriver.save_screenshot('final3.png')
         sub_measure()
     measure()
