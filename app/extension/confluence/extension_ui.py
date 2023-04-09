10162b6ebedff1fdd36f0d4aaaad479635983a0f
v.nezhdvetsky <v.nezhdvetsky@softlist.ua>
2023-04-09 16:39:28 +0300

    custom action

--- app/extension/confluence/extension_ui.py
+++ app/extension/confluence/extension_ui.py
@@ -10,35 +10,25 @@ from util.conf import CONFLUENCE_SETTINGS
 
 def app_specific_action(webdriver, datasets):
     page = BasePage(webdriver)
-    if datasets['custom_pages']:
-        app_specific_page_id = datasets['custom_page_id']
 
-    # To run action as specific user uncomment code bellow.
-    # NOTE: If app_specific_action is running as specific user, make sure that app_specific_action is running
-    # just before test_2_selenium_z_log_out
-    # @print_timing("selenium_app_specific_user_login")
-    # def measure():
-    #     def app_specific_user_login(username='admin', password='admin'):
-    #         login_page = Login(webdriver)
-    #         login_page.delete_all_cookies()
-    #         login_page.go_to()
-    #         login_page.wait_for_page_loaded()
-    #         login_page.set_credentials(username=username, password=password)
-    #         login_page.click_login_button()
-    #         if login_page.is_first_login():
-    #             login_page.first_user_setup()
-    #         all_updates_page = AllUpdates(webdriver)
-    #         all_updates_page.wait_for_page_loaded()
-    #     app_specific_user_login(username='admin', password='admin')
-    # measure()
-
-    @print_timing("selenium_app_custom_action")
+    @print_timing("selenium_instantsearch")
     def measure():
 
-        @print_timing("selenium_app_custom_action:view_page")
-        def sub_measure():
-            page.go_to_url(f"{CONFLUENCE_SETTINGS.server_url}/pages/viewpage.action?pageId={app_specific_page_id}")
-            page.wait_until_visible((By.ID, "title-text"))  # Wait for title field visible
-            page.wait_until_visible((By.ID, "ID_OF_YOUR_APP_SPECIFIC_UI_ELEMENT"))  # Wait for you app-specific UI element by ID selector
-        sub_measure()
+        @print_timing("selenium_instantsearch:view_log_table_page")
+        def view_log_table_page():
+            page.go_to_url(f"{CONFLUENCE_SETTINGS.server_url}/display/INSTASEARCH/instantsearchlogtable")
+            page.wait_until_visible((By.ID, "popularTable"))
+            page.wait_until_visible((By.ID, "failedTable"))
+            page.wait_until_visible((By.ID, "userSearchTable"))
+        view_log_table_page()
+
+
+        @print_timing("selenium_instantsearch:view_macro_page_and_search")
+        def view_macro_page_and_search():
+            page.go_to_url(f"{CONFLUENCE_SETTINGS.server_url}/display/INSTASEARCH/instantsearchmacro")
+            page.wait_until_visible((By.CLASS_NAME, "plugin_instantsearch_searchbox"))
+            page.get_element((By.CLASS_NAME, "plugin_instantsearch_searchbox")).send_keys("page")
+            page.wait_until_visible((By.CLASS_NAME, "plugin_instantsearch_returnedSearchResult"))
+        view_macro_page_and_search()
+
     measure()
