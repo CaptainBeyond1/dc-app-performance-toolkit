6b3f31bd9ff8d0dc1ba60081d9841ec0e27b8648
Vitaly Nezdvetskiy <vnezdd@gmail.com>
2024-01-04 20:51:58 +0200

    action naming

--- app/extension/jira/extension_ui.py
+++ app/extension/jira/extension_ui.py
@@ -33,7 +33,7 @@ def app_specific_action(webdriver, datasets):
 
     @print_timing("selenium_app_custom_action")
     def measure():
-        @print_timing("selenium_app_custom_action:view_issue")
+        @print_timing("selenium_app_custom_action:create_request")
         def sub_measure():
             actions = ActionChains(webdriver)
             page.go_to_url(f"{JIRA_SETTINGS.server_url}/secure/CallViewAction.jspa?reporter=admin&testfield={testfieldValue}")
