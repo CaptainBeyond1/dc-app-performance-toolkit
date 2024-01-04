ee579322e03348136fcdc8819c1cb0715c4a2d6a
Vitaly Nezdvetskiy <vnezdd@gmail.com>
2024-01-04 19:37:58 +0200

    extention

--- app/extension/jira/extension_ui.py
+++ app/extension/jira/extension_ui.py
@@ -1,44 +1,51 @@
+import string
 import random
 
 from selenium.webdriver.common.by import By
+from selenium.webdriver import ActionChains
 
 from selenium_ui.base_page import BasePage
 from selenium_ui.conftest import print_timing
+from selenium.webdriver.common.keys import Keys
 from selenium_ui.jira.pages.pages import Login
 from util.conf import JIRA_SETTINGS
 
 
 def app_specific_action(webdriver, datasets):
     page = BasePage(webdriver)
-    if datasets['custom_issues']:
-        issue_key = datasets['custom_issue_key']
+    letters = string.ascii_lowercase
+    testfieldValue = ''.join(random.choice(letters) for i in range(10))
 
-    # To run action as specific user uncomment code bellow.
-    # NOTE: If app_specific_action is running as specific user, make sure that app_specific_action is running
-    # just before test_2_selenium_z_log_out action
-    #
-    # @print_timing("selenium_app_specific_user_login")
-    # def measure():
-    #     def app_specific_user_login(username='admin', password='admin'):
-    #         login_page = Login(webdriver)
-    #         login_page.delete_all_cookies()
-    #         login_page.go_to()
-    #         login_page.set_credentials(username=username, password=password)
-    #         if login_page.is_first_login():
-    #             login_page.first_login_setup()
-    #         if login_page.is_first_login_second_page():
-    #             login_page.first_login_second_page_setup()
-    #         login_page.wait_for_page_loaded()
-    #     app_specific_user_login(username='admin', password='admin')
-    # measure()
+    @print_timing("selenium_app_specific_user_login")
+    def measure():
+        def app_specific_user_login(username='admin', password='admin'):
+            login_page = Login(webdriver)
+            login_page.delete_all_cookies()
+            login_page.go_to()
+            login_page.set_credentials(username=username, password=password)
+            if login_page.is_first_login():
+                login_page.first_login_setup()
+            if login_page.is_first_login_second_page():
+                login_page.first_login_second_page_setup()
+            login_page.wait_for_page_loaded()
+        app_specific_user_login(username='admin', password='admin')
+    measure()
 
     @print_timing("selenium_app_custom_action")
     def measure():
         @print_timing("selenium_app_custom_action:view_issue")
         def sub_measure():
-            page.go_to_url(f"{JIRA_SETTINGS.server_url}/browse/{issue_key}")
-            page.wait_until_visible((By.ID, "summary-val"))  # Wait for summary field visible
-            page.wait_until_visible((By.ID, "ID_OF_YOUR_APP_SPECIFIC_UI_ELEMENT"))  # Wait for you app-specific UI element by ID selector
+            actions = ActionChains(webdriver)
+            page.go_to_url(f"{JIRA_SETTINGS.server_url}/secure/CallViewAction.jspa?reporter=admin&testfield={testfieldValue}")
+            page.wait_until_visible((By.ID, "create_link"))
+            page.get_element((By.ID, "create_link")).click()
+            page.wait_until_visible((By.ID, "summary"))
+            page.get_element((By.ID, "summary")).click()
+            actions.send_keys(''.join(random.choice(letters) for i in range(10))).perform();
+            page.get_element((By.ID, "reporter-field")).click()
+            page.get_element((By.ID, "create-issue-submit")).click()
+            page.wait_until_visible((By.CLASS_NAME, "aui-close-button"))
+            page.get_element((By.CLASS_NAME, "aui-close-button")).click()
         sub_measure()
     measure()
 
